0167b43e5efadbd4035deb4e516af586ac6d3989
eisa22 <einspieler.work@gmail.com>
2026-01-17 13:57:15 +0100

    ID refactoring

--- Visualization/visualizer.py
+++ Visualization/visualizer.py
@@ -220,71 +220,118 @@ def visualize_3d_with_ids(session_path, masks, labels):
         
         geoms.append(pcd_surface)
     
-    # Füge ID-Labels als flache weiße Scheiben hinzu
-    for mask_id, center, color in mask_centers:
-        # Erstelle flache weiße Scheibe (2D) als Marker
-        disc = o3d.geometry.TriangleMesh.create_cylinder(radius=0.05, height=0.002)
-        disc.translate(center)
-        disc.paint_uniform_color([1.0, 1.0, 1.0])  # Weiße Scheibe
-        disc.compute_vertex_normals()
-        geoms.append(disc)
-    
     # Zeige die Visualisierung mit Labels
     print("\n[VIZ] ID-Zuordnung:")
     for mask_id, center, color in mask_centers:
         print(f"  ID {mask_id}: Position ({center[0]:.2f}, {center[1]:.2f}, {center[2]:.2f})")
     
-    # Verwende Open3D Visualizer mit 3D-Labels
-    vis = o3d.visualization.Visualizer()
-    vis.create_window(window_name="3/3: Segmentierte Objekte mit IDs", width=1280, height=720)
+    # Ziffern-Muster definieren (5x7 Grid) - DICHTERE Muster
+    digit_patterns = {
+        '1': [(1,0),(2,0),(2,1),(2,2),(2,3),(2,4),(2,5),(1,6),(2,6),(3,6),(2,0),(2,1)],
+        '2': [(0,0),(1,0),(2,0),(3,0),(4,0),(4,1),(4,2),(3,3),(2,3),(1,4),(0,5),(0,6),(1,6),(2,6),(3,6),(4,6)],
+        '3': [(0,0),(1,0),(2,0),(3,0),(4,1),(4,2),(2,3),(3,3),(4,4),(4,5),(0,6),(1,6),(2,6),(3,6)],
+        '4': [(0,0),(0,1),(0,2),(0,3),(1,3),(2,3),(3,3),(4,3),(3,0),(3,1),(3,2),(3,4),(3,5),(3,6)],
+        '5': [(0,0),(1,0),(2,0),(3,0),(4,0),(0,1),(0,2),(0,3),(1,3),(2,3),(3,3),(4,4),(4,5),(0,6),(1,6),(2,6),(3,6)],
+        '6': [(1,0),(2,0),(3,0),(0,1),(0,2),(0,3),(1,3),(2,3),(3,3),(0,4),(4,4),(0,5),(4,5),(1,6),(2,6),(3,6)],
+        '7': [(0,0),(1,0),(2,0),(3,0),(4,0),(4,1),(3,2),(3,3),(2,4),(2,5),(2,6)],
+        '8': [(1,0),(2,0),(3,0),(0,1),(4,1),(0,2),(4,2),(1,3),(2,3),(3,3),(0,4),(4,4),(0,5),(4,5),(1,6),(2,6),(3,6)],
+        '9': [(1,0),(2,0),(3,0),(0,1),(4,1),(0,2),(4,2),(1,3),(2,3),(3,3),(4,3),(4,4),(4,5),(1,6),(2,6),(3,6)],
+        '0': [(1,0),(2,0),(3,0),(0,1),(4,1),(0,2),(4,2),(0,3),(4,3),(0,4),(4,4),(0,5),(4,5),(1,6),(2,6),(3,6)],
+    }
     
-    for geom in geoms:
-        vis.add_geometry(geom)
+    # Erstelle Billboard-Labels (Scheibe + Text) die zur Kamera ausgerichtet werden
+    billboard_data = []  # (center, disc_mesh, label_pcd, base_disc_vertices, base_label_points)
     
-    # Füge 3D-Labels hinzu (rote Schrift direkt auf der Scheibe)
-    for mask_id, center, color in mask_centers:
+    for mask_id, center, _ in mask_centers:
+        # Erstelle Scheibe - Position so dass sie aus der Oberfläche heraussteht
+        disc = o3d.geometry.TriangleMesh.create_cylinder(radius=0.05, height=0.002)
+        # Offset: Scheibe steht aus der Oberfläche heraus (in Kamera-Richtung / +Z)
+        disc_offset = center.copy()
+        disc_offset[2] += 0.05  # 5cm über der Oberfläche
+        disc.translate(disc_offset)
+        disc.paint_uniform_color([1.0, 1.0, 1.0])
+        disc.compute_vertex_normals()
+        base_disc_vertices = np.asarray(disc.vertices).copy()
+        
+        # Erstelle Label-Punkte - DICHTER mit mehr Punkten pro Position
         label_points = []
         label_colors = []
-        red = [1.0, 0.0, 0.0]  # Rot statt neon-grün
+        red = [1.0, 0.0, 0.0]
         
-        # Ziffern-Muster definieren (5x7 Grid)
-        digit_patterns = {
-            '1': [(2,0),(1,1),(2,1),(2,2),(2,3),(2,4),(2,5),(1,6),(2,6),(3,6)],
-            '2': [(1,0),(2,0),(3,0),(0,1),(4,1),(4,2),(3,3),(2,4),(1,5),(0,6),(1,6),(2,6),(3,6),(4,6)],
-            '3': [(0,0),(1,0),(2,0),(3,0),(4,1),(3,2),(2,3),(3,4),(4,5),(0,6),(1,6),(2,6),(3,6)],
-            '4': [(0,0),(3,0),(0,1),(3,1),(0,2),(3,2),(0,3),(1,3),(2,3),(3,3),(4,3),(3,4),(3,5),(3,6)],
-            '5': [(0,0),(1,0),(2,0),(3,0),(4,0),(0,1),(0,2),(1,2),(2,2),(3,2),(4,3),(4,4),(0,5),(1,6),(2,6),(3,6)],
-            '6': [(1,0),(2,0),(3,0),(0,1),(0,2),(0,3),(1,3),(2,3),(3,3),(0,4),(4,4),(0,5),(4,5),(1,6),(2,6),(3,6)],
-            '7': [(0,0),(1,0),(2,0),(3,0),(4,0),(4,1),(3,2),(2,3),(2,4),(2,5),(2,6)],
-            '8': [(1,0),(2,0),(3,0),(0,1),(4,1),(0,2),(4,2),(1,3),(2,3),(3,3),(0,4),(4,4),(0,5),(4,5),(1,6),(2,6),(3,6)],
-            '9': [(1,0),(2,0),(3,0),(0,1),(4,1),(0,2),(4,2),(1,3),(2,3),(3,3),(4,3),(4,4),(4,5),(1,6),(2,6),(3,6)],
-            '0': [(1,0),(2,0),(3,0),(0,1),(4,1),(0,2),(4,2),(0,3),(4,3),(0,4),(4,4),(0,5),(4,5),(1,6),(2,6),(3,6)],
-        }
-        
-        # Konvertiere ID zu String und erstelle Punkte
         id_str = str(mask_id)
         num_digits = len(id_str)
-        scale = 0.006  # Etwas kleiner für die Scheibe
+        scale = 0.008  # Größere Skalierung
         total_width = num_digits * 5 * scale
         
         offset_x = 0
         for char in id_str:
             if char in digit_patterns:
                 for (px, py) in digit_patterns[char]:
-                    point = center.copy()
-                    point[0] += (px + offset_x) * scale - total_width / 2  # Zentriert
-                    point[1] -= (py - 3) * scale  # Vertikal zentriert
-                    point[2] += 0.003  # Direkt auf der Scheibe
-                    label_points.append(point)
-                    label_colors.append(red)
-            offset_x += 6  # Abstand zwischen Ziffern
+                    # Relative Position zur Mitte
+                    # X: NEGIERT für korrekte Lesbarkeit (von vorne gesehen)
+                    rel_x = -((px + offset_x) * scale - total_width / 2)
+                    rel_y = (py - 3) * scale
+                    # Mehrere Punkte pro Position für dichtere Darstellung (feinere Auflösung)
+                    for dx in [-0.0005, 0, 0.0005]:
+                        for dy in [-0.0005, 0, 0.0005]:
+                            # Vorderseite
+                            label_points.append([rel_x + dx, rel_y + dy, 0.005])
+                            label_colors.append(red)
+                            # Rückseite (X nochmal gespiegelt)
+                            label_points.append([-rel_x - dx, rel_y + dy, -0.005])
+                            label_colors.append(red)
+            offset_x += 6
         
+        label_pcd = o3d.geometry.PointCloud()
+        label_colors_arr = np.array(label_colors) if label_colors else np.array([[1, 0, 0]])
         if label_points:
-            label_pcd = o3d.geometry.PointCloud()
-            label_pcd.points = o3d.utility.Vector3dVector(np.array(label_points))
-            label_pcd.colors = o3d.utility.Vector3dVector(np.array(label_colors))
+            base_label_points = np.array(label_points)
+            label_pcd.points = o3d.utility.Vector3dVector(base_label_points + disc_offset)
+            label_pcd.colors = o3d.utility.Vector3dVector(label_colors_arr)
+        else:
+            base_label_points = np.array([[0, 0, 0]])
+        
+        billboard_data.append((disc_offset, disc, label_pcd, base_disc_vertices, base_label_points, label_colors_arr))
+        geoms.append(disc)
+    
+    # Verwende Open3D Visualizer mit Animation Callback für Billboard-Effekt
+    vis = o3d.visualization.Visualizer()
+    vis.create_window(window_name="3/3: Segmentierte Objekte mit IDs", width=1280, height=720)
+    
+    for geom in geoms:
+        vis.add_geometry(geom)
+    
+    for _, _, label_pcd, _, base_label_pts, _ in billboard_data:
+        if len(base_label_pts) > 1 or (len(base_label_pts) == 1 and not np.allclose(base_label_pts[0], [0, 0, 0])):
             vis.add_geometry(label_pcd)
     
+    def update_billboards(vis):
+        """Callback um Billboards zur Kamera auszurichten."""
+        ctr = vis.get_view_control()
+        cam = ctr.convert_to_pinhole_camera_parameters()
+        extrinsic = cam.extrinsic
+        
+        # Kamera-Position und Blickrichtung extrahieren
+        R = extrinsic[:3, :3]
+        # Rotation Matrix um Billboards zur Kamera auszurichten
+        
+        for center, disc, label_pcd, base_disc_verts, base_label_pts, label_colors_arr in billboard_data:
+            # Rotiere Scheibe zur Kamera
+            rotated_verts = (R.T @ (base_disc_verts - center).T).T + center
+            disc.vertices = o3d.utility.Vector3dVector(rotated_verts)
+            disc.compute_vertex_normals()
+            vis.update_geometry(disc)
+            
+            # Rotiere Label-Punkte und stelle Farben wieder her
+            if len(base_label_pts) > 1 or (len(base_label_pts) == 1 and not np.allclose(base_label_pts[0], [0, 0, 0])):
+                rotated_pts = (R.T @ base_label_pts.T).T + center
+                label_pcd.points = o3d.utility.Vector3dVector(rotated_pts)
+                label_pcd.colors = o3d.utility.Vector3dVector(label_colors_arr)
+                vis.update_geometry(label_pcd)
+        
+        return False
+    
+    vis.register_animation_callback(update_billboards)
     vis.run()
     vis.destroy_window()
 
